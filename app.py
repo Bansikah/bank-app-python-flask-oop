e8f16b6f8f55bf31047f045b01571fb055eca221
kouamschekina <kouamdelphine82@gmail.com>
2024-02-10 12:30:25 +0100

    feat(deposit functionality): Implementation of the deposit form, deposit logic and balance update

--- app.py
+++ app.py
@@ -7,37 +7,65 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 @app.route('/deposit', methods=['GET', 'POST'])
+
 def deposit_form():
-    if request.method == 'POST':
-        account_number = request.values.get('account-number')
-        deposit_amount = float(request.values.get('deposit_amount'))
-
-        updated_balance = update_balance(account_number, deposit_amount)
-
-        return "Deposit of {} successfully processed for account number {}. <br>Updated balance: {}".format(deposit_amount, account_number, updated_balance)
-
-    return render_template('deposit.html')
+    try: 
+        
+        if request.method == 'POST': 
+            account_number = request.values.get('account-number')
+            deposit_amount = float(request.values.get('deposit_amount'))
+            if deposit_amount <= 0 :
+                raise ValueError("Invalid Amount.")
+            
+            updated_balance = update_balance(account_number, deposit_amount)
+            if updated_balance is None:
+                raise ValueError("Invalid account number.")
+            return "Deposit of {}FCFA successfully processed😊👍. <br>Updated balance: {}FCFA".format(deposit_amount, updated_balance)
+        return render_template('deposit.html')
+    
+    except ValueError as ve:
+        error_message = f"Error: {ve}"
+        return f'<div style="color: red;">{error_message}</div>'
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {e}"
+        return f'<div style="color: red;">{error_message}</div>'
 
 # Function to update balance in the CSV file
 def update_balance(account_number, deposit_amount):
-    with open('data.csv', mode='r') as file:
-        reader = csv.DictReader(file)
-        accounts = list(reader)
+    try:
+        with open('data.csv', mode='r') as file:
+            reader = csv.DictReader(file)
+            accounts = list(reader)
 
-    for account in accounts:
-        if account['account_number'] == account_number:
-            current_balance = float(account['balance'].replace(',', ''))
-            new_balance = current_balance + deposit_amount
-            account['balance'] = str(new_balance)
-            updated_balance = new_balance
+        for account in accounts:
+            if account['account_number'] == account_number:
+                current_balance = float(account['balance'].replace(',', ''))
+                new_balance = current_balance + deposit_amount
+                account['balance'] = str(new_balance)
+                updated_balance = new_balance
+                break
+            else:
+                return None
 
-    with open('data.csv', mode='w', newline='') as file:
-        fieldnames = ['account_number', 'balance']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(accounts)
-    return updated_balance
+
+        with open('data.csv', mode='w', newline='') as file:
+            fieldnames = ['account_number', 'balance']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(accounts)
+        return updated_balance
+    
+    except ValueError as ve:
+        print(f"Error: {ve}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+         
+    
 if __name__ == '__main__':
     app.run(debug=True)
